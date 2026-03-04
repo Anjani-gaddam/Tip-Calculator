376f0710a193b0ac542ddd5e300393d54a2eb32d
Anjani-gaddam <anjanigaddam585@gmail.com>
2026-03-04 23:07:08 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+let billAmountElement = document.getElementById("billAmount");
+let percentageTipElement = document.getElementById("percentageTip");
+let tipAmountElement = document.getElementById("tipAmount");
+let totalAmountElement = document.getElementById("totalAmount");
+let errorMessageElement = document.getElementById("errorMessage");
+
+function calculateTip() {
+    if (billAmountElement.value === "") {
+        errorMessageElement.textContent = "Please enter a valid input";
+    } else if (percentageTipElement.value === "") {
+        errorMessageElement.textContent = "Please enter a valid input";
+    } else {
+        errorMessageElement.textContent = "";
+        let billAmount = parseInt(billAmountElement.value);
+        let percentageTip = parseInt(percentageTipElement.value);
+
+        let calculatedTip = (percentageTip / 100) * billAmount;
+        let totalAmount = billAmount + calculatedTip;
+
+        tipAmountElement.value = calculatedTip;
+        totalAmountElement.value = totalAmount;
+
+    }
+}
